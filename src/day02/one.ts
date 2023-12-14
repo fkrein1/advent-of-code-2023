357a6b920497857ea07894905310e393a9fc5c2f
Felipe Krein Rocha <felipekrein@gmail.com>
2023-12-14 10:32:14 -0300

    feat: day 02

---
+++ src/day02/one.ts
@@ -0,0 +1,30 @@
+import { data } from './input';
+
+let total = 0;
+
+for (let line of data) {
+  const [game, cubes] = line.split(':');
+  const gameNumber = Number(game.replace('Game ', ''));
+  const gameDraw = cubes.split(';');
+  let count = 0;
+  for (let draw of gameDraw) {
+    const dict: { [key: string]: number } = {};
+    const cubes_list = draw.split(',');
+    for (let cube of cubes_list) {
+      const [qty, color] = cube.trim().split(' ');
+      dict[color] = Number(qty);
+    }
+    if (
+      (dict['red'] <= 12 || !('red' in dict)) &&
+      (dict['green'] <= 13 || !('green' in dict)) &&
+      (dict['blue'] <= 14 || !('blue' in dict))
+    ) {
+      count += 1;
+    }
+    if (count === gameDraw.length) {
+      total += gameNumber;
+    }
+  }
+}
+
+console.log(total);
---
+++ src/day02/two.ts
@@ -0,0 +1,27 @@
+import { data } from './input';
+
+let total = 0;
+
+for (let line of data) {
+  const [, cubes] = line.split(':');
+  const gameDraw = cubes.split(';');
+  const dict_game: { [key: string]: number } = {};
+  for (let draw of gameDraw) {
+    const cubes_list = draw.split(',');
+    for (let cube of cubes_list) {
+      const [qty, color] = cube.trim().split(' ');
+      if (color in dict_game) {
+        dict_game[color] =
+          dict_game[color] > Number(qty) ? dict_game[color] : Number(qty);
+      } else {
+        dict_game[color] = Number(qty);
+      }
+    }
+  }
+  total +=
+    (dict_game['red'] ?? 0) *
+    (dict_game['green'] ?? 0) *
+    (dict_game['blue'] ?? 0);
+}
+
+console.log(total);
